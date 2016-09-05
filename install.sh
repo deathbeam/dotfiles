b8a839fefc33cbdd38b054abbfd9fc0827ffc478
Tomas Slusny <slusnucky@gmail.com>
2016-09-05 22:33:19 +0200

    Add possibility of bash-it configuration

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Starting installation. Patience you must have my young padawan."
 
-ARC_HOME=~/.awesomedotrc
+ARC_HOME=$HOME/.awesomedotrc
 ARC_RCS="$ARC_HOME/dotrcs"
 ARC_USER="$ARC_HOME/user"
 ARC_LIB="$ARC_HOME/.lib"
@@ -25,30 +25,50 @@ cd $ARC_HOME
   git clone https://github.com/junegunn/vim-plug $ARC_LIB/autoload
 
 # Install Bash config
-echo "source $HOME/.bashrc" >> ~/.bash_profile
-echo "ARC_HOME=$ARC_HOME
+echo "
+[[ -f $ARC_USER/bashitrc ]] &&
+  source $ARC_USER/bashitrc
+
+[[ -z \"\$BASH_IT\" ]] &&
+  export BASH_IT=\"$ARC_LIB/bashit\"
+[[ -z \"\$BASH_IT_THEME\" ]] &&
+  export BASH_IT_THEME='minimal'
+[[ -z \"\$SCM_CHECK\" ]] &&
+  export SCM_CHECK=true
+
+source \$BASH_IT/bash_it.sh
+source $HOME/.bashrc
+" > $HOME/.bash_profile
+
+echo "
+ARC_HOME=$ARC_HOME
 ARC_USER=$ARC_USER
 ARC_LIB=$ARC_LIB
+
 source $ARC_RCS/bashrc
-[[ -f $ARC_USER/bashrc ]] && source $ARC_USER/bashrc
-" >> ~/.bashrc
+[[ -f $ARC_USER/bashrc ]] &&
+  source $ARC_USER/bashrc
+" > $HOME/.bashrc
 
 # Install Vim config
-echo "set runtimepath+=$ARC_LIB
+echo "
+set runtimepath+=$ARC_LIB
 let \$ARC_USER='$ARC_USER'
 let g:plug_home=fnamemodify(expand('$ARC_LIB/plugged'), ':p')
+
 source $ARC_RCS/vimrc
 try
 source $ARC_USER/vimrc
 catch
 endtry
-" >> ~/.vimrc
+" > $HOME/.vimrc
 
 # Install Vim plugins
 vim +PlugInstall +qall
 
 # Fix MacVim fullscreen mode
-command -v mvim >/dev/null 2>&1 && defaults write org.vim.MacVim MMNativeFullScreen 0
+command -v mvim >/dev/null 2>&1 &&
+  defaults write org.vim.MacVim MMNativeFullScreen 0
 
 # Make NeoVim use same config as Vim
 rm -rf ~/.config/nvim
@@ -56,4 +76,5 @@ mkdir -p ~/.config/nvim
 mkdir -p $ARC_LIB/autoload
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
+# Reload terminal after installation
 [[ ! $TERM == "dumb" ]] && bash
