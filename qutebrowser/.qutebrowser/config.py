49493f11e20e4cb033ae17d29bfa14fa8473d145
Tomas <Tomas@Tomass-MacBook-Pro.local>
2017-10-25 00:42:27 +0200

    Add mpv spawning to qutebrowser
    Signed-off-by: Tomas <Tomas@Tomass-MacBook-Pro.local>

--- qutebrowser/.qutebrowser/config.py
+++ qutebrowser/.qutebrowser/config.py
@@ -1,3 +1,4 @@
+# Adjust font
 c.fonts.completion.category = "bold 16pt monospace"
 c.fonts.completion.entry = "16pt monospace"
 c.fonts.debug_console = "16pt monospace"
@@ -11,3 +12,7 @@ c.fonts.monospace = "\"xos4 Terminus\", Terminus, monospace"
 c.fonts.prompts = "16pt sans-serif"
 c.fonts.statusbar = "16pt monospace"
 c.fonts.tabs = "16pt monospace"
+
+# Play videos with mpv
+config.bind('e', 'spawn mpv {url}')
+config.bind('E', 'hint links spawn mpv {hint-url}')
