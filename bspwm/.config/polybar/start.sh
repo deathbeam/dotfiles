0390ae4b7423b961a8117bb50f84986e011e72e9
Tomas Slusny <slusnucky@gmail.com>
2018-12-12 08:37:00 +0100

    Fix polybar and HiDPI dpi
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

---
+++ bspwm/.config/polybar/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Start polybar
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload main &
+done
