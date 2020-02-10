787e22574587c5c91b19d873dc3496365e38f787
Tomas Slusny <slusnucky@gmail.com>
2020-02-10 21:45:21 +0100

    Fix clipboard, update quotebrowser, update dependencies
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -3,19 +3,21 @@ c.content.plugins = True
 c.qt.args = ['ppapi-widevine-path=/usr/lib/qt/plugins/ppapi/libwidevinecdmadapter.so']
 
 # Adjust font and font size
-c.fonts.completion.category = "bold 16pt monospace"
-c.fonts.completion.entry = "16pt monospace"
-c.fonts.debug_console = "16pt monospace"
-c.fonts.downloads = "16pt monospace"
-c.fonts.hints = "16pt monospace"
-c.fonts.keyhint = "16pt monospace"
-c.fonts.messages.error = "16pt monospace"
-c.fonts.messages.info = "16pt monospace"
-c.fonts.messages.warning = "16pt monospace"
-c.fonts.monospace = "Terminus, monospace"
-c.fonts.prompts = "16pt sans-serif"
-c.fonts.statusbar = "16pt monospace"
-c.fonts.tabs = "16pt monospace"
+c.fonts.completion.category = "bold 16pt Terminus"
+c.fonts.completion.entry = "16pt Terminus"
+c.fonts.debug_console = "16pt Terminus"
+c.fonts.downloads = "16pt Terminus"
+c.fonts.hints = "16pt Terminus"
+c.fonts.keyhint = "16pt Terminus"
+c.fonts.messages.error = "16pt Terminus"
+c.fonts.messages.info = "16pt Terminus"
+c.fonts.messages.warning = "16pt Terminus"
+c.fonts.default_family = "Terminus, monospace"
+c.fonts.default_size = "16pt"
+c.fonts.prompts = "16pt Terminus"
+c.fonts.statusbar = "16pt Terminus"
+c.fonts.tabs = "16pt Terminus"
+# c.fonts.monospace = "Terminus, monospace"
 
 # Tabs
 c.tabs.padding = {"top": 5, "bottom": 5, "left": 5, "right": 5}
@@ -25,7 +27,7 @@ config.bind('e', 'spawn mpv {url}')
 config.bind('E', 'hint links spawn mpv {hint-url}')
 
 # base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
-# Base16 qutebrowser template by theova
+# Base16 qutebrowser template by theova and Daniel Mulford
 # Solarized Dark scheme by Ethan Schoonover (modified by aramisgithub)
 
 base00 = "#002b36"
@@ -58,7 +60,7 @@ c.colors.completion.odd.bg = base00
 c.colors.completion.even.bg = base00
 
 # Foreground color of completion widget category headers.
-c.colors.completion.category.fg = base0A
+c.colors.completion.category.fg = base0D
 
 # Background color of the completion widget category headers.
 c.colors.completion.category.bg = base00
@@ -70,25 +72,40 @@ c.colors.completion.category.border.top = base00
 c.colors.completion.category.border.bottom = base00
 
 # Foreground color of the selected completion item.
-c.colors.completion.item.selected.fg = base01
+c.colors.completion.item.selected.fg = base00
 
 # Background color of the selected completion item.
-c.colors.completion.item.selected.bg = base0A
+c.colors.completion.item.selected.bg = base0D
 
-# Top border color of the completion widget category headers.
-c.colors.completion.item.selected.border.top = base0A
+# Top border color of the selected completion item
+c.colors.completion.item.selected.border.top = base0D
 
 # Bottom border color of the selected completion item.
-c.colors.completion.item.selected.border.bottom = base0A
+c.colors.completion.item.selected.border.bottom = base0D
+
+# Foreground color of the matched text in the selected completion item.
+c.colors.completion.item.selected.match.fg = base00
 
 # Foreground color of the matched text in the completion.
-c.colors.completion.match.fg = base0B
+c.colors.completion.match.fg = base09
 
 # Color of the scrollbar handle in the completion view.
 c.colors.completion.scrollbar.fg = base05
 
 # Color of the scrollbar in the completion view.
 c.colors.completion.scrollbar.bg = base00
+
+# Background color of the context menu. If set to null, the Qt default is used.
+c.colors.contextmenu.menu.bg = base00
+
+# Foreground color of the context menu. If set to null, the Qt default is used.
+c.colors.contextmenu.menu.fg =  base05
+
+# Background color of the context menu’s selected item. If set to null, the Qt default is used.
+c.colors.contextmenu.selected.bg = base0D
+
+#Foreground color of the context menu’s selected item. If set to null, the Qt default is used.
+c.colors.contextmenu.selected.fg = base00
 
 # Background color for the download bar.
 c.colors.downloads.bar.bg = base00
@@ -167,52 +184,52 @@ c.colors.prompts.bg = base00
 c.colors.prompts.selected.bg = base0A
 
 # Foreground color of the statusbar.
-c.colors.statusbar.normal.fg = base0B
+c.colors.statusbar.normal.fg = base05
 
 # Background color of the statusbar.
 c.colors.statusbar.normal.bg = base00
 
 # Foreground color of the statusbar in insert mode.
-c.colors.statusbar.insert.fg = base00
+c.colors.statusbar.insert.fg = base0C
 
 # Background color of the statusbar in insert mode.
-c.colors.statusbar.insert.bg = base0D
+c.colors.statusbar.insert.bg = base00
 
 # Foreground color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.fg = base00
+c.colors.statusbar.passthrough.fg = base0A
 
 # Background color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.bg = base0C
+c.colors.statusbar.passthrough.bg = base00
 
 # Foreground color of the statusbar in private browsing mode.
-c.colors.statusbar.private.fg = base00
+c.colors.statusbar.private.fg = base0E
 
 # Background color of the statusbar in private browsing mode.
-c.colors.statusbar.private.bg = base03
+c.colors.statusbar.private.bg = base00
 
 # Foreground color of the statusbar in command mode.
-c.colors.statusbar.command.fg = base05
+c.colors.statusbar.command.fg = base04
 
 # Background color of the statusbar in command mode.
-c.colors.statusbar.command.bg = base00
+c.colors.statusbar.command.bg = base01
 
 # Foreground color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.fg = base05
+c.colors.statusbar.command.private.fg = base0E
 
 # Background color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.bg = base00
+c.colors.statusbar.command.private.bg = base01
 
 # Foreground color of the statusbar in caret mode.
-c.colors.statusbar.caret.fg = base00
+c.colors.statusbar.caret.fg = base0D
 
 # Background color of the statusbar in caret mode.
-c.colors.statusbar.caret.bg = base0E
+c.colors.statusbar.caret.bg = base00
 
 # Foreground color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.fg = base00
+c.colors.statusbar.caret.selection.fg = base0D
 
 # Background color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.bg = base0D
+c.colors.statusbar.caret.selection.bg = base00
 
 # Background color of the progress bar.
 c.colors.statusbar.progress.bg = base0D
@@ -224,11 +241,11 @@ c.colors.statusbar.url.fg = base05
 c.colors.statusbar.url.error.fg = base08
 
 # Foreground color of the URL in the statusbar for hovered links.
-c.colors.statusbar.url.hover.fg = base05
+c.colors.statusbar.url.hover.fg = base09
 
 # Foreground color of the URL in the statusbar on successful load
 # (http).
-c.colors.statusbar.url.success.http.fg = base0C
+c.colors.statusbar.url.success.http.fg = base0B
 
 # Foreground color of the URL in the statusbar on successful load
 # (https).
@@ -261,14 +278,42 @@ c.colors.tabs.even.fg = base05
 # Background color of unselected even tabs.
 c.colors.tabs.even.bg = base00
 
+# Background color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.bg = base0B
+
+# Foreground color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.fg = base00
+
+# Background color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.bg = base0B
+
+# Foreground color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.fg = base00
+
+# Background color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.bg = base0D
+
+# Foreground color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.fg = base00
+
+# Background color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.bg = base0D
+
+# Foreground color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.fg = base00
+
 # Foreground color of selected odd tabs.
 c.colors.tabs.selected.odd.fg = base00
 
 # Background color of selected odd tabs.
-c.colors.tabs.selected.odd.bg = base05
+c.colors.tabs.selected.odd.bg = base0D
 
 # Foreground color of selected even tabs.
 c.colors.tabs.selected.even.fg = base00
 
 # Background color of selected even tabs.
-c.colors.tabs.selected.even.bg = base05
+c.colors.tabs.selected.even.bg = base0D
+
+# Background color for webpages if unset (or empty to use the theme's
+# color).
+c.colors.webpage.bg = base00
