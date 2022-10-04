68555c885f15b2e1680cdb0a532ec2d6cef9d1ad
Tomas Slusny <slusnucky@gmail.com>
2022-10-05 00:24:21 +0200

    Fix completion
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- zsh/.zsh/bundle/completions.plugin.zsh
+++
@@ -1,9 +0,0 @@
-# if it's a dumb terminal, return.
-if [[ ${TERM} == 'dumb' ]]; then
-  return 1
-fi
-
-fpath=(${0:h}/completions ${fpath})
-
-# load and initialize the completion system
-autoload -Uz compinit && compinit -C -d "${ZDOTDIR:-${HOME}}/${zcompdump_file:-.zcompdump}"
