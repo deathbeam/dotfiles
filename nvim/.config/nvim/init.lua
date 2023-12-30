ed1278742bd469549767ed301c7b3b5877245408
Tomas Slusny <slusnucky@gmail.com>
2023-12-30 05:25:28 +0100

    Add cursor workaround for leap
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,6 +8,21 @@ vim.cmd([[
 -- Motions
 require('leap').create_default_mappings()
 require('flit').setup()
+-- Hide the (real) cursor when leaping, and restore it afterwards.
+vim.api.nvim_create_autocmd('User', { pattern = 'LeapEnter',
+    callback = function()
+      vim.cmd.hi('Cursor', 'blend=100')
+      vim.opt.guicursor:append { 'a:Cursor/lCursor' }
+    end,
+  }
+)
+vim.api.nvim_create_autocmd('User', { pattern = 'LeapLeave',
+    callback = function()
+      vim.cmd.hi('Cursor', 'blend=0')
+      vim.opt.guicursor:remove { 'a:Cursor/lCursor' }
+    end,
+  }
+)
 
 -- Utility functions
 local nmap = function(keys, func, desc, buffer)
