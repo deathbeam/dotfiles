f9c060c26765eb146158be51fb42c9cb0fe0a31f
Tomas Slusny <slusnucky@gmail.com>
2024-01-11 20:36:04 +0100

    Add tab complete and snip complete

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,6 +10,16 @@ local nmap = function(keys, func, desc, buffer)
   vim.keymap.set('n', keys, func, { buffer = buffer, desc = desc })
 end
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 -- Icons
 require('nvim-web-devicons').setup()
 
@@ -153,43 +163,11 @@ nmap('<leader>fb', '<cmd>FzfLua buffers<cr>', '[F]ind [B]uffers')
 nmap('<leader>fh', '<cmd>FzfLua oldfiles<cr>', '[F]ind [H]istory')
 
 -- Completion
-local cmp_kinds = {
-  Text = '  ',
-  Method = '  ',
-  Function = '  ',
-  Constructor = '  ',
-  Field = '  ',
-  Variable = '  ',
-  Class = '  ',
-  Interface = '  ',
-  Module = '  ',
-  Property = '  ',
-  Unit = '  ',
-  Value = '  ',
-  Enum = '  ',
-  Keyword = '  ',
-  Snippet = '  ',
-  Color = '  ',
-  File = '  ',
-  Reference = '  ',
-  Folder = '  ',
-  EnumMember = '  ',
-  Constant = '  ',
-  Struct = '  ',
-  Event = '  ',
-  Operator = '  ',
-  TypeParameter = '  ',
-}
+vim.g.codeium_disable_bindings = 1
+vim.keymap.set('i', '<C-e>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
 
 local cmp = require('cmp')
 cmp.setup {
-  formatting = {
-    fields = { "kind", "abbr" },
-    format = function(_, vim_item)
-      vim_item.kind = cmp_kinds[vim_item.kind] or ""
-      return vim_item
-    end,
-  },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'vsnip' },
@@ -202,7 +180,26 @@ cmp.setup {
       vim.fn["vsnip#anonymous"](args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert {
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      elseif cmp.visible() then
+        cmp.select_prev_item()
+      end
+    end, { "i", "s" }),
     ['<C-n>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
