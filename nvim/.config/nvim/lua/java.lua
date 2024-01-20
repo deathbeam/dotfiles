b79ebb392dc9128ed9ffb8318bcd10f414304d18
Tomas Slusny <slusnucky@gmail.com>
2024-01-20 01:59:46 +0100

    More cleanup

--- nvim/.config/nvim/lua/java.lua
+++ nvim/.config/nvim/lua/java.lua
@@ -4,7 +4,7 @@ local cache_vars = {}
 
 local features = {
   -- change this to `true` to enable codelens
-  codelens = false,
+  codelens = true,
 
   -- change this to `true` if you have `nvim-dap`,
   -- `java-test` and `java-debug-adapter` installed
@@ -103,16 +103,6 @@ local function jdtls_on_attach(client, bufnr)
   if features.codelens then
     enable_codelens(bufnr)
   end
-
-  -- The following mappings are based on the suggested usage of nvim-jdtls
-  -- https://github.com/mfussenegger/nvim-jdtls#usage
-  local opts = {buffer = bufnr}
-  vim.keymap.set('n', '<A-o>', "<cmd>lua require('jdtls').organize_imports()<cr>", opts)
-  vim.keymap.set('n', 'crv', "<cmd>lua require('jdtls').extract_variable()<cr>", opts)
-  vim.keymap.set('x', 'crv', "<esc><cmd>lua require('jdtls').extract_variable(true)<cr>", opts)
-  vim.keymap.set('n', 'crc', "<cmd>lua require('jdtls').extract_constant()<cr>", opts)
-  vim.keymap.set('x', 'crc', "<esc><cmd>lua require('jdtls').extract_constant(true)<cr>", opts)
-  vim.keymap.set('x', 'crm', "<esc><Cmd>lua require('jdtls').extract_method(true)<cr>", opts)
 end
 
 local function jdtls_setup()
