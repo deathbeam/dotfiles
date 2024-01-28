210aba2f9bfb5dfd47867daa131f021a239b6010
Tomas Slusny <slusnucky@gmail.com>
2024-01-28 21:03:38 +0100

    Fix python-dap

--- nvim/.config/nvim/lua/config/languages/python.lua
+++ nvim/.config/nvim/lua/config/languages/python.lua
@@ -1,14 +1,13 @@
 local nmap = require('config/utils').nmap
 local registry = require('mason-registry')
 local python_dap = require('dap-python')
+local path = registry.get_package('debugpy'):get_install_path()
+path = path .. '/venv/bin/python'
+python_dap.setup(path)
 
 local function python_setup()
-    local path = registry.get_package('debugpy'):get_install_path()
-    path = path .. '/venv/bin/python'
-    python_dap.setup(path)
-    local bufnr = 0
-    nmap('<leader>dt', python_dap.test_method, '[D]ebug [T]est Method', bufnr)
-    nmap('<leader>dT', python_dap.test_class, '[D]ebug [T]est Class', bufnr)
+    nmap('<leader>dt', python_dap.test_method, '[D]ebug [T]est Method', 0)
+    nmap('<leader>dT', python_dap.test_class, '[D]ebug [T]est Class', 0)
 end
 
 vim.api.nvim_create_autocmd('FileType', {
