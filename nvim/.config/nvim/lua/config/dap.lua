2d18d895892e844b4696301970b2f59620510995
Tomas Slusny <ts6234@att.com>
2024-02-07 17:02:13 +0100

    Do not auto close dap on exit
    Its annoying

--- nvim/.config/nvim/lua/config/dap.lua
+++ nvim/.config/nvim/lua/config/dap.lua
@@ -45,8 +45,6 @@ dapui.setup {
 
 dap.listeners.before.attach.dapui_config = function() dapui.open({ reset = true }) end
 dap.listeners.before.launch.dapui_config = function() dapui.open({ reset = true }) end
-dap.listeners.before.event_terminated.dapui_config = dapui.close
-dap.listeners.before.event_exited.dapui_config = dapui.close
 
 vim.api.nvim_create_autocmd("VimResized", {
     desc = "DAP UI resize",
@@ -56,7 +54,6 @@ vim.api.nvim_create_autocmd("VimResized", {
         end
     end
 })
-
 
 rnmap("<leader>d<space>", function() if dap.session() then dap.continue() end end, "[D]ebug Continue")
 rnmap("<leader>dj", dap.step_over, "[D]ebug Step Over (down)")
