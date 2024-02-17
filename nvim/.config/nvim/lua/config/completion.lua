796847f8a463760dc8ff12c264a3c23a6932b396
Tomas Slusny <slusnucky@gmail.com>
2024-02-17 22:12:08 +0100

    Update autocomplete and disable <CR> FUCK YOU CR

--- nvim/.config/nvim/lua/config/completion.lua
+++ nvim/.config/nvim/lua/config/completion.lua
@@ -15,14 +15,17 @@ require('copilot').setup({
   },
 })
 
--- Completion
+-- Enable popup menu and set options
 vim.o.completeopt = 'menuone,noinsert,popup'
+
+-- Tab to accept
 vim.keymap.set("i", "<Tab>", function()
-    if vim.fn.pumvisible() ~= 0 then
-        return "<C-y>"
-    else
-        return "<Tab>"
-    end
+    return vim.fn.pumvisible() ~= 0 and "<C-y>" or "<Tab>"
+end, { expr = true, replace_keycodes = true })
+
+-- Disable <CR> to accept (this really should be a mapping, so stupid)
+vim.keymap.set("i", "<CR>", function()
+    return vim.fn.pumvisible() ~= 0 and "<Esc>o" or "<CR>"
 end, { expr = true, replace_keycodes = true })
 
 require('autocomplete.signature').setup { border = "single" }
