d58f18b699fab7473dd82de9a5db58dd35d225b5
Tomas Slusny <slusnucky@gmail.com>
2024-02-18 22:29:15 +0100

    Add back entry icons to completion

--- nvim/.config/nvim/lua/config/completion.lua
+++ nvim/.config/nvim/lua/config/completion.lua
@@ -1,3 +1,5 @@
+local icons = require('config.icons')
+
 -- Copilot autosuggestions
 require('copilot').setup({
     panel = {
@@ -28,8 +30,19 @@ vim.keymap.set('i', '<CR>', function()
     return vim.fn.pumvisible() ~= 0 and '<Esc>o' or '<CR>'
 end, { expr = true, replace_keycodes = true })
 
-require('autocomplete.signature').setup({ border = 'single' })
-require('autocomplete.lsp').setup({})
+require('autocomplete.signature').setup({
+    border = 'single'
+})
+
+require('autocomplete.lsp').setup({
+    entry_mapper = function(entry)
+        local kind = entry.kind
+        local icon = icons.kinds[kind]
+        entry.kind = icon and icon .. ' ' .. kind or kind
+        return entry
+    end,
+})
+
 require('autocomplete.cmd').setup({
     mappings = {
         accept = '<Tab>',
