d9e273c372eb995f35ddc34a9dda56db3e34e841
Tomas Slusny <ts6234@att.com>
2024-02-20 09:59:37 +0100

    Add more prompts

--- nvim/.config/nvim/lua/config/copilot.lua
+++ nvim/.config/nvim/lua/config/copilot.lua
@@ -6,6 +6,43 @@ vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, rep
 -- Copilot chat
 require("CopilotChat").setup({})
 
-vim.keymap.set('n', '<leader>ce', '<cmd>CopilotChatExplain<CR>', { desc = "[C]ode [E]xplain" })
+local prompts = {
+    {
+        prompt = 'Explain how it works.',
+        desc = '[E]explain',
+        key = 'e'
+    },
+    {
+        prompt = 'Briefly explain how selected code works then generate unit tests.',
+        desc = 'Generate [T]ests',
+        key = 't'
+    },
+    {
+        prompt = 'Briefly explain how selected code works then generate documentation.',
+        desc = '[D]ocumentation',
+        key = 'd'
+    },
+    {
+        prompt = 'Find possible errors and fix them for me.',
+        desc = '[F]ix',
+        key = 'f'
+    },
+    {
+        prompt = 'Optimize the code to improve performance and readablilty.',
+        desc = '[O]ptimize',
+        key = 'o'
+    },
+    {
+        prompt = 'Simplify and improve readablilty',
+        desc = '[S]implify',
+        key = 's'
+    },
+
+}
+
 vim.keymap.set('n', '<leader>cd', '<cmd>CopilotChatFixDiagnostic<CR>', { desc = "[C]ode Fix [D]iagnostic" })
-vim.keymap.set('n', '<leader>ct', '<cmd>CopilotChatTests<CR>', { desc = "[C]ode Generate [T]ests" })
+
+for _, prompt in ipairs(prompts) do
+    vim.keymap.set('n', string.format('<leader>a%s', prompt.key), string.format('<cmd>CopilotChat %s<CR>', prompt.prompt), { desc = "[A]I " .. prompt.desc })
+    vim.keymap.set('v', string.format('<leader>a%s', prompt.key), string.format(':CopilotChatVisual %s<CR>', prompt.prompt), { desc = "[A]I " .. prompt.desc })
+end
