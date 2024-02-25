5a6311d73d0c01cc11b93e77a9b8ce06cb7b6189
Tomas Slusny <slusnucky@gmail.com>
2024-02-25 06:27:09 +0100

    Mark active sessions in session switcher

--- tmux/.tmux/switch-session.sh
+++ tmux/.tmux/switch-session.sh
@@ -1,14 +1,37 @@
 #!/usr/bin/env bash
 
-selected_project=$(find ~/git -mindepth 1 -maxdepth 1 -type d | fzf-tmux \
-    -p100%,100% -m --reverse \
+tmux_sessions=$(tmux list-sessions -F '#S')
+all_sessions=$(find ~/git -mindepth 1 -maxdepth 1 -type d | sort)
+
+function session_name() {
+    basename $1 | tr . _
+}
+
+function get_marked_sessions() {
+    for session in $all_sessions; do
+        local name="$(session_name $session)"
+        if [[ $tmux_sessions == *$name* ]]; then
+            echo -e "\033[0;34m$session\033[0m"
+        fi
+    done
+    for session in $all_sessions; do
+        local name="$(session_name $session)"
+        if [[ $tmux_sessions != *$name* ]]; then
+            echo $session
+        fi
+    done
+}
+
+selected_project=$(get_marked_sessions | fzf-tmux \
+    --ansi -p100%,100% -m --reverse \
     --prompt='Open session > ' \
-    --preview="tree -C -L 3 --dirsfirst {}")
+    --preview='tree -C -L 3 --dirsfirst {}')
 
 if [[ -z $selected_project ]]; then
     exit 0
 fi
 
+# Remove * from selected project
 selected_name=$(basename "$selected_project" | tr . _)
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
