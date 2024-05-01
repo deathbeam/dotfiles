115635676d6b9850a463b76d25f106308e8f825e
Tomas Slusny <ts6234@att.com>
2024-05-01 23:23:47 +0200

    Fix lemonbar dep

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,7 +52,7 @@ yay --noconfirm -S --mflags --skipinteg \
   feh zathura zathura-pdf-mupdf imagemagick \
   qutebrowser python-adblock chromium-widevine \
   libnotify dunst \
-  bspwm sxhkd lemonbar-xft \
+  bspwm sxhkd lemonbar-xft-git \
   geoclue2 redshift \
   udiskie \
   dropbox \
