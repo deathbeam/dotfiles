1e7a147d169a0faf17f63906fcac39ac7fe2afd8
Tomas Slusny <slusnucky@gmail.com>
2024-05-03 00:52:03 +0200

    Add yambar config, simplify bspwm config

--- bootstrap-wayland.sh
+++ bootstrap-wayland.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash -l
 set -ex
 
-yay --noconfirm -S --mflags --skipinteg hyprland xdg-desktop-portal-hyprland swaybg wl-clipboard
+yay --noconfirm -S --mflags --skipinteg hyprland xdg-desktop-portal-hyprland swaybg swaylock wl-clipboard yambar
