44620aa73ada8f6ca49112c51c393725da546f51
Tomas Slusny <slusnucky@gmail.com>
2024-05-04 23:28:55 +0200

    Add asus stuff to bootstarp-nvidia.sh

--- bootstrap-nvidia.sh
+++ bootstrap-nvidia.sh
@@ -6,3 +6,22 @@ yay --noconfirm -S --mflags --skipinteg \
     nvidia-beta nvidia-utils-beta lib32-nvidia-utils-beta
 
 sudo systemctl enable nvidia-powerd.service
+
+echo '==> Setting up g14 repository'
+sudo tee -a /etc/pacman.conf <<EOF
+[g14]
+Server = https://arch.asus-linux.org
+EOF
+
+pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+
+echo '==> Installing Asus packages'
+yay --noconfirm -Sy --mflags --skipinteg \
+    asusctl power-profiles-daemon supergfxctl switcheroo-control rog-control-center
+
+sudo systemctl enable power-profiles-daemon
+sudo systemctl enable supergfxd
+sudo systemctl enable switcheroo-control
