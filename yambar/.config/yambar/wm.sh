5ce1b1ef34a5fad6a577bfad95c04400047a4fe7
Tomas Slusny <slusnucky@gmail.com>
2024-05-06 21:43:18 +0200

    Use elif

--- yambar/.config/yambar/wm.sh
+++ yambar/.config/yambar/wm.sh
@@ -4,8 +4,7 @@ scriptpath=$(dirname $(realpath $0))
 
 if [ -n "$(pgrep -x bspwm)" ]; then
     $scriptpath/bspwm.sh
-fi
-
-if [ -n "$(pgrep -x Hyprland)" ]; then
+elif [ -n "$(pgrep -x Hyprland)" ]; then
     $scriptpath/hyprland.sh
 fi
+
