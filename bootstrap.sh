3a10a0eb523f857c6fdd15358b68dc0e21cc38cb
Tomas Slusny <slusnucky@gmail.com>
2024-05-06 19:39:44 +0200

    add fastfetch

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,7 @@ yay --noconfirm -S --mflags --skipinteg \
   pass pass-otp zbar \
   httpie sshpass stoken openvpn vpn-slice openconnect wget jq \
   tlp rate-mirrors unzip fuse2 bc brightnessctl \
-  p7zip man-db alsa-utils keyd
+  p7zip man-db alsa-utils keyd fastfetch
 
 echo '==> Installing development packages'
 yay --noconfirm -S --mflags --skipinteg \
