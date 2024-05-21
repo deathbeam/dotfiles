a039c9126d023426ffc64a47c65f66c4a29310b9
Tomas Slusny <slusnucky@gmail.com>
2024-05-21 22:39:19 +0200

    Improve wayland setup, improve nvidia-beta setup

--- bootstrap-nvidia.sh
+++ bootstrap-nvidia.sh
@@ -3,7 +3,10 @@ set -ex
 
 echo '==> Installing Nvidia packages'
 yay -S --noconfirm --mflags --skipinteg \
-    nvidia-beta nvidia-utils-beta lib32-nvidia-utils-beta libva-nvidia-driver
+    nvidia-beta nvidia-settings-beta \
+    nvidia-utils-beta lib32-nvidia-utils-beta \
+    opencl-nvidia-beta lib32-opencl-nvidia-beta \
+    libva-nvidia-driver
 
 echo '==> Configuring Nvidia'
 sudo systemctl enable \
