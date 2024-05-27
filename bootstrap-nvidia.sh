e670b78d2cd8e47fc0d946c332308ced1fde170e
Tomas Slusny <slusnucky@gmail.com>
2024-05-27 19:34:05 +0200

    Use dkms for nvidia

--- bootstrap-nvidia.sh
+++ bootstrap-nvidia.sh
@@ -3,7 +3,7 @@ set -ex
 
 echo '==> Installing Nvidia packages'
 yay -S --noconfirm --mflags --skipinteg \
-    nvidia-beta nvidia-settings-beta \
+    dkms nvidia-beta-dkms nvidia-settings-beta \
     nvidia-utils-beta lib32-nvidia-utils-beta \
     opencl-nvidia-beta lib32-opencl-nvidia-beta \
     libva-nvidia-driver
