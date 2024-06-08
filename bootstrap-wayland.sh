9cbba2ab2567233dd3b0d8624c731874f27e8ac9
Tomas Slusny <slusnucky@gmail.com>
2024-06-09 00:08:51 +0200

    Update submodules

--- bootstrap-wayland.sh
+++ bootstrap-wayland.sh
@@ -3,5 +3,5 @@ set -ex
 
 echo '==> Installing Wayland packages'
 yay -S --noconfirm --mflags --skipinteg \
-    hyprland-git xorg-xwayland-git xdg-desktop-portal-hyprland qt6-wayland \
+    hyprwayland-scanner-git hyprland-git xorg-xwayland-git xdg-desktop-portal-hyprland qt6-wayland \
     swaybg swaylock wl-clipboard cliphist slurp grim
