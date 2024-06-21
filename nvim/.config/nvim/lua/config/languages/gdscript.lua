5b09c6e5dc9df12cb6db4b4a5b66dfa4fd7050e9
Tomas Slusny <slusnucky@gmail.com>
2024-06-21 17:24:54 +0200

    Add godot config for dap

---
+++ nvim/.config/nvim/lua/config/languages/gdscript.lua
@@ -0,0 +1,15 @@
+local dap = require('dap')
+local utils = require('config.utils')
+local au = utils.au
+
+au('FileType', {
+    pattern = { 'gdscript' },
+    desc = 'Setup gdscript',
+    callback = function()
+        dap.adapters.godot = {
+            type = "server",
+            host = '127.0.0.1',
+            port = 6006,
+        }
+    end,
+})
