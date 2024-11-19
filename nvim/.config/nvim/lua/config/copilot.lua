153acc26158915c03cb6710b5ba48de1d3fe2935
Tomas Slusny <slusnucky@gmail.com>
2024-11-19 18:37:27 +0100

    chore: update copilot config and dependencies
    Change copilot chat log level from debug to warn and update CopilotChat.nvim
    and nvim-jdtls dependencies to their latest versions.

--- nvim/.config/nvim/lua/config/copilot.lua
+++ nvim/.config/nvim/lua/config/copilot.lua
@@ -15,7 +15,7 @@ local select = require('CopilotChat.select')
 local integration = require('CopilotChat.integrations.fzflua')
 
 chat.setup({
-    log_level = 'debug',
+    log_level = 'warn',
     model = 'claude-3.5-sonnet',
     question_header = ' ' .. icons.ui.User .. ' ',
     answer_header = ' ' .. icons.ui.Bot .. ' ',
