852e18a1012940f60c25abd69cb17cddb01fc376
Tomas Slusny <slusnucky@gmail.com>
2024-12-05 09:59:05 +0100

    update submodules, fix mappings
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/lua/config/utils.lua
+++ nvim/.config/nvim/lua/config/utils.lua
@@ -11,7 +11,7 @@ M.dot = function(callback)
     end
 end
 
-M.map = function(mode, keys, func, dot, desc, buffer)
+M.map = function(mode, keys, func, desc, buffer, dot)
     if dot then
         if func then
             local f = func
@@ -28,27 +28,27 @@ M.map = function(mode, keys, func, dot, desc, buffer)
 end
 
 M.nmap = function(keys, func, desc, buffer)
-    M.map('n', keys, func, false, desc, buffer)
+    M.map('n', keys, func, desc, buffer)
 end
 
 M.rnmap = function(keys, func, desc, buffer)
-    M.map('n', keys, func, true, desc, buffer)
+    M.map('n', keys, func, desc, buffer, true)
 end
 
 M.vmap = function(keys, func, desc, buffer)
-    M.map('v', keys, func, false, desc, buffer)
+    M.map('v', keys, func, desc, buffer)
 end
 
 M.rvmap = function(keys, func, desc, buffer)
-    M.map('v', keys, func, true, desc, buffer)
+    M.map('v', keys, func, desc, buffer, true)
 end
 
 M.nvmap = function(keys, func, desc, buffer)
-    M.map({ 'n', 'v' }, keys, func, false, desc, buffer)
+    M.map({ 'n', 'v' }, keys, func, desc, buffer)
 end
 
 M.rnvmap = function(keys, func, desc, buffer)
-    M.map({ 'n', 'v' }, keys, func, true, desc, buffer)
+    M.map({ 'n', 'v' }, keys, func, desc, buffer, true)
 end
 
 M.au = function(event, opts)
