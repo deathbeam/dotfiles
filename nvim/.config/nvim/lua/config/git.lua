90d0c477ca5abb1224141ed1e6d6b8dc4b61d5b3
Tomas Slusny <slusnucky@gmail.com>
2025-01-22 22:29:52 +0100

    update git bindings
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/lua/config/git.lua
+++ nvim/.config/nvim/lua/config/git.lua
@@ -6,13 +6,13 @@ local nmap = utils.nmap
 local vmap = utils.vmap
 local desc = utils.desc
 
-desc('<leader>h', 'Hunk', icons.ui.Hunk)
+desc('<leader>g', 'Git', icons.ui.Hunk)
 
 gitsigns.setup({
     signcolumn = false,
     numhl = true,
     on_attach = function(bufnr)
-        nmap(']c', function()
+        nmap(']g', function()
             if vim.wo.diff then
                 vim.cmd.normal({ ']c', bang = true })
             else
@@ -20,7 +20,7 @@ gitsigns.setup({
             end
         end, 'Goto next hunk', bufnr)
 
-        nmap('[c', function()
+        nmap('[g', function()
             if vim.wo.diff then
                 vim.cmd.normal({ '[c', bang = true })
             else
@@ -29,28 +29,28 @@ gitsigns.setup({
         end, 'Goto previous hunk', bufnr)
 
         -- Actions
-        nmap('<leader>hs', gitsigns.stage_hunk, 'Stage hunk', bufnr)
-        vmap('<leader>hs', function()
+        nmap('<leader>gs', gitsigns.stage_hunk, 'Stage hunk', bufnr)
+        vmap('<leader>gs', function()
             gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
         end, 'Stage hunk', bufnr)
-        nmap('<leader>hr', gitsigns.reset_hunk, 'Reset hunk', bufnr)
-        vmap('<leader>hr', function()
+        nmap('<leader>gS', gitsigns.stage_buffer, 'Stage buffer', bufnr)
+        nmap('<leader>gr', gitsigns.reset_hunk, 'Reset hunk', bufnr)
+        vmap('<leader>gr', function()
             gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
         end, 'Reset hunk', bufnr)
-        nmap('<leader>hS', gitsigns.stage_buffer, 'Stage buffer', bufnr)
-        nmap('<leader>hu', gitsigns.undo_stage_hunk, 'Undo stage hunk', bufnr)
-        nmap('<leader>hR', gitsigns.reset_buffer, 'Reset buffer', bufnr)
-        nmap('<leader>hp', gitsigns.preview_hunk, 'Preview hunk', bufnr)
-        nmap('<leader>hb', function()
+        nmap('<leader>gR', gitsigns.reset_buffer, 'Reset buffer', bufnr)
+        nmap('<leader>gu', gitsigns.undo_stage_hunk, 'Undo stage hunk', bufnr)
+        nmap('<leader>gp', gitsigns.preview_hunk, 'Preview hunk', bufnr)
+        nmap('<leader>gb', function()
             gitsigns.blame_line({ full = true })
         end, 'Blame line', bufnr)
-        nmap('<leader>hB', gitsigns.blame, 'Blame', bufnr)
-        nmap('<leader>hd', gitsigns.diffthis, 'Diff this', bufnr)
-        nmap('<leader>hD', function()
+        nmap('<leader>gB', gitsigns.blame, 'Blame', bufnr)
+        nmap('<leader>gd', gitsigns.diffthis, 'Diff this', bufnr)
+        nmap('<leader>gD', function()
             gitsigns.diffthis('~')
         end, 'Diff this (cached)', bufnr)
 
         -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'Select hunk', bufnr)
+        map({ 'o', 'x' }, 'ig', ':<C-U>Gitsigns select_hunk<CR>', 'Select hunk', bufnr)
     end,
 })
