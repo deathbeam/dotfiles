41eba77eafa37fa6577db1f863cddf9e4955de96
Tomas Slusny <slusnucky@gmail.com>
2025-03-01 01:46:40 +0100

    add support for github notifications
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- yambar/.config/yambar/notifications.sh
+++ yambar/.config/yambar/notifications.sh
@@ -10,4 +10,9 @@ aur_updates=$(yay -Qum 2>/dev/null | wc -l)
 total_updates=$((official_updates + aur_updates))
 
 echo "updates|int|$total_updates"
+
+github_notifications=$(gh api notifications --jq 'length')
+
+echo "github|int|$github_notifications"
+
 echo ""
