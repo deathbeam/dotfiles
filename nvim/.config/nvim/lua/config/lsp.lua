aab04d1cc9f9d0d5d962d048003d6a684bc942af
Tomas Slusny <slusnucky@gmail.com>
2025-03-10 19:03:42 +0100

    check if float window isnt hidden
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -52,7 +52,7 @@ au('CursorHold', {
 
         for _, win in ipairs(vim.api.nvim_list_wins()) do
             local config = vim.api.nvim_win_get_config(win)
-            if config.relative ~= '' then
+            if config.relative ~= '' and not config.hide then
                 local buf = vim.api.nvim_win_get_buf(win)
                 if buf and vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
                     return
