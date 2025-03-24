d0f3b61e3d901358af9975ef610b3c7271df7bf8
Tomas Slusny <slusnucky@gmail.com>
2025-03-25 00:51:51 +0100

    remove copilot.vim workaround
    its fixed in latest copilot.vim version
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/lua/config/copilot.lua
+++ nvim/.config/nvim/lua/config/copilot.lua
@@ -6,7 +6,6 @@ utils.desc('<leader>a', 'AI')
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_hide_during_completion = false
 vim.g.copilot_proxy_strict_ssl = false
-vim.g.copilot_integration_id = 'vscode-chat'
 vim.g.copilot_settings = { selectedCompletionModel = 'gpt-4o-copilot' }
 vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
 
