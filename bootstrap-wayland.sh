ee37c5a2ddcbbb21fd69a55e17f018f47dd32343
Tomas Slusny <slusnucky@gmail.com>
2025-04-01 18:34:54 +0200

    use non git hyprland
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- bootstrap-wayland.sh
+++ bootstrap-wayland.sh
@@ -2,9 +2,6 @@
 set -ex
 
 echo '==> Installing Wayland packages'
-yay -S --asdeps --noconfirm --mflags --skipinteg \
-    hyprutils-git hyprlang-git hyprcursor-git hyprwayland-scanner-git
-
 yay -S --noconfirm --mflags --skipinteg \
-    hyprland-git xorg-xwayland xclip xdg-desktop-portal-hyprland qt6-wayland \
+    hyprland xorg-xwayland xclip xdg-desktop-portal-hyprland qt5-wayland qt6-wayland \
     swaybg swaylock wl-clipboard cliphist slurp grim hyprpicker grimblast-git
