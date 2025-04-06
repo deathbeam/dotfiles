aef1b3b9332b01630c90f9e47429c757bddf0763
Tomas Slusny <slusnucky@gmail.com>
2025-04-06 14:55:09 +0200

    add gemini
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/lua/config/copilot.lua
+++ nvim/.config/nvim/lua/config/copilot.lua
@@ -145,14 +145,22 @@ chat.setup({
     },
     providers = {
         github_models = {
-            disabled = false,
+            disabled = true,
         },
-        ollama = {
+        gemini = {
             prepare_input = require('CopilotChat.config.providers').copilot.prepare_input,
             prepare_output = require('CopilotChat.config.providers').copilot.prepare_output,
 
+            get_headers = function()
+                local api_key = assert(os.getenv('GEMINI_API_KEY'), 'GEMINI_API_KEY env not set')
+                return {
+                    Authorization = 'Bearer ' .. api_key,
+                    ['Content-Type'] = 'application/json',
+                }
+            end,
+
             get_models = function(headers)
-                local response, err = require('CopilotChat.utils').curl_get('http://localhost:11434/v1/models', {
+                local response, err = require('CopilotChat.utils').curl_get('https://generativelanguage.googleapis.com/v1beta/openai/models', {
                     headers = headers,
                     json_response = true,
                 })
@@ -162,21 +170,22 @@ chat.setup({
                 end
 
                 return vim.tbl_map(function(model)
+                    local id = model.id:gsub('^models/', '')
                     return {
-                        id = model.id,
-                        name = model.id,
+                        id = id,
+                        name = id,
                     }
                 end, response.body.data)
             end,
 
             embed = function(inputs, headers)
-                local response, err = require('CopilotChat.utils').curl_post('http://localhost:11434/v1/embeddings', {
+                local response, err = require('CopilotChat.utils').curl_post('https://generativelanguage.googleapis.com/v1beta/openai/embeddings', {
                     headers = headers,
                     json_request = true,
                     json_response = true,
                     body = {
                         input = inputs,
-                        model = 'all-minilm',
+                        model = 'text-embedding-004',
                     },
                 })
 
@@ -188,7 +197,7 @@ chat.setup({
             end,
 
             get_url = function()
-                return 'http://localhost:11434/v1/chat/completions'
+                return 'https://generativelanguage.googleapis.com/v1beta/openai/chat/completions'
             end,
         }
     },
