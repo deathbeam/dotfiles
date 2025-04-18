288283f6e2c5f9718a80e3699283aa6f1e0c4468
Tomas Slusny <slusnucky@gmail.com>
2025-04-18 05:27:49 +0200

    improve bar display
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- yambar/.config/yambar/gpu.sh
+++ yambar/.config/yambar/gpu.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 if [ command -v nvidia-smi &> /dev/null ]; then
-    echo "percent|int|-1"
+    echo "utilization|int|-1"
+    echo "temperature|int|-1"
     exit 1
 fi
 
-nvidia-smi -lms 1000 --query-gpu=utilization.gpu --format=csv,noheader,nounits | while read -r line; do
-    echo "percent|int|$line"
+nvidia-smi -lms 1000 --query-gpu=utilization.gpu,temperature.gpu --format=csv,noheader,nounits | while read -r line; do
+    utilization=$(echo "$line" | cut -d ',' -f 1 | tr -d ' ')
+    temperature=$(echo "$line" | cut -d ',' -f 2 | tr -d ' ')
+    echo "utilization|int|$utilization"
+    echo "temperature|int|$temperature"
     echo ""
 done
