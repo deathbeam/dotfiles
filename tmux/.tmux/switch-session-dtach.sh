d862602119f20fee90cf8c67d676319b697550d7
Tomas Slusny <slusnucky@gmail.com>
2025-08-09 23:20:47 +0200

    add some scripts for tmux alternatives
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

---
+++ tmux/.tmux/switch-session-dtach.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+dtach_sessions=$(find /tmp -maxdepth 1 -type s -name 'dtach-*' 2>/dev/null | sed 's|/tmp/dtach-||')
+project_directories=$(find ~/git -mindepth 1 -maxdepth 1 -type d ; find ~ -mindepth 1 -maxdepth 1 -type d -name '[A-Z]*')
+
+function session_name() {
+    basename "$1" | tr . _
+}
+
+function directory_name() {
+    basename "$1" | tr _ .
+}
+
+function get_marked_sessions() {
+    # Sessions not matching a directory
+    while IFS= read -r session; do
+        dir_name="$(directory_name "$session")"
+        if ! echo "$project_directories" | grep -q "$dir_name"; then
+            echo -e "\033[0;36m$session\033[0m"
+        fi
+    done <<< "$dtach_sessions"
+
+    # Directories, mark those with active sessions
+    while IFS= read -r directory; do
+        sess_name="$(session_name "$directory")"
+        if echo "$dtach_sessions" | grep -q "^${sess_name}$"; then
+            echo -e "\033[0;36m$directory\033[0m"
+        else
+            echo "$directory"
+        fi
+    done <<< "$project_directories"
+}
+
+selected_project=$(get_marked_sessions | sort | fzf \
+    --ansi -m --reverse \
+    --prompt='Open session > ' \
+    --bind="ctrl-s:print-query" \
+    --header='<ctrl-s> to use query' \
+    --preview='
+        session_name=$(basename {} | tr . _)
+        if [ -e /tmp/dtach-$session_name ]; then
+            echo "Active dtach session: $session_name"
+        else
+            ls --group-directories-first --color=always -lahG {}
+        fi
+    ')
+
+if [[ -z $selected_project ]]; then
+    exit 0
+fi
+
+selected_name=$(session_name "$selected_project")
+socket="/tmp/dtach-$selected_name"
+
+exec dtach -A "$socket" -r winch $SHELL -c "cd '$selected_project'; exec $SHELL"
