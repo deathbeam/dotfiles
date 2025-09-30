9eeedcbe1fc0d517ec7c9954d2bedc9b2f6710de
Tomas Slusny <slusnucky@gmail.com>
2025-09-30 23:31:03 +0200

    improve lua ls add config reference
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/after/lsp/lua_ls.lua
+++ nvim/.config/nvim/after/lsp/lua_ls.lua
@@ -1,8 +1,15 @@
 return {
     settings = {
+        -- See: https://luals.github.io/wiki/settings/
         Lua = {
             runtime = {
                 version = 'LuaJIT',
+                -- Tell the language server how to find Lua modules same way as Neovim
+                -- (see `:h lua-module-load`)
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
             },
             hint = {
                 enable = true,
