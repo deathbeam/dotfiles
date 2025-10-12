f8b2700e59731306ada4d1d09bdada40018eb508
Tomas Slusny <slusnucky@gmail.com>
2025-10-12 23:04:50 +0200

    remove unused myplugins stuff
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/lua/config/myplugins.lua
+++ nvim/.config/nvim/lua/config/myplugins.lua
@@ -4,17 +4,6 @@ local nmap = utils.nmap
 local desc = utils.desc
 
 require('myplugins').setup({
-    -- bufcomplete = {
-    --     entry_mapper = function(entry)
-    --         local kind = entry.kind
-    --         local icon = icons.kinds[kind]
-    --         entry.kind = icon and icon .. ' ' .. kind or kind
-    --         return entry
-    --     end,
-    -- },
-    -- cmdcomplete = {},
-    -- difftool = {},
-
     lspdocs = {},
     lspecho = {
         attach_log = true,
@@ -50,9 +39,6 @@ nmap('<leader>wt', wiki.today, 'Wiki Today')
 nmap('<leader>wd', wiki.list_diary, 'Wiki Diary List')
 nmap('<leader>ww', wiki.list_wiki, 'Wiki List')
 nmap('<leader>wn', wiki.new, 'Wiki New')
-
--- local undotree = require('myplugins.undotree')
--- nmap('<leader>fu', undotree.show, 'Find Undo History')
 
 local zoom = require('myplugins.zoom')
 nmap('<leader>z', zoom.toggle, 'Zoom')
