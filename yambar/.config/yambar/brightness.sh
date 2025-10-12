e54bc22fdcd9b5f84b02b65427d24d02a658be61
Tomas Slusny <slusnucky@gmail.com>
2025-10-12 23:09:56 +0200

    remove alacritty, yambar and satty configs
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- yambar/.config/yambar/brightness.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-percent=$(brightnessctl -m | awk -F, '{print $4}' | tr -d '%')
-echo "brightness|int|$percent"
-echo ""
--- yambar/.config/yambar/wm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-scriptpath=$(dirname $(realpath $0))
-
-if [ -n "$(pgrep -x Hyprland)" ]; then
-    exec $scriptpath/hyprland.sh
-fi
-
