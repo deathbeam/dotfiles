c7fb23b33c7efc422e1222c9f0dfe54a193eaca6
Tomas Slusny <slusnucky@gmail.com>
2026-04-15 01:21:52 +0200

    optimize stuff
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- zsh/.zsh/pack/bundle/start/passenv.plugin.zsh
+++ zsh/.zsh/pack/bundle/start/passenv.plugin.zsh
@@ -1,20 +1,23 @@
 CACHE_FILE="$HOME/.cache/pass_env"
 CACHE_AGE_MIN=60
 
-# Refresh cache if missing or older than CACHE_AGE_MIN
-if [ ! -f "$CACHE_FILE" ] || [ "$(find "$CACHE_FILE" -mmin +$CACHE_AGE_MIN)" ]; then
-  print -n > "$CACHE_FILE"
-
-  prefix=${PASSWORD_STORE_DIR-~/.password-store}
-  password_files=( "$prefix"/Env/**/*.gpg )
-  password_files=( "${password_files[@]#"$prefix"/}" )
-  password_files=( "${password_files[@]%.gpg}" )
-  for password_file in "${password_files[@]}"; do
-    varname=$(basename "$password_file")
-    value="$(pass "$password_file")"
-    print -r -- "export $varname='$value'" >> "$CACHE_FILE"
-  done
+# Source the cache file if it exists
+if [[ -f "$CACHE_FILE" ]]; then
+  source "$CACHE_FILE"
 fi
 
-# Source cached exports
-source "$CACHE_FILE"
+# Refresh cache if missing or older than CACHE_AGE_MIN, in background
+if [ ! -f "$CACHE_FILE" ] || [ "$(find "$CACHE_FILE" -mmin +$CACHE_AGE_MIN)" ]; then
+  (
+    prefix=${PASSWORD_STORE_DIR-~/.password-store}
+    password_files=( "$prefix"/Env/**/*.gpg )
+    password_files=( "${password_files[@]#"$prefix"/}" )
+    password_files=( "${password_files[@]%.gpg}" )
+    > "$CACHE_FILE"
+    for password_file in "${password_files[@]}"; do
+      varname=$(basename "$password_file")
+      value="$(pass "$password_file")"
+      print -r -- "export $varname='$value'" >> "$CACHE_FILE"
+    done
+  ) &
+fi
