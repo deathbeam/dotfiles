45c072021e5fdf5d6c4122251233d30a348e23b2
Tomas Slusny <slusnucky@gmail.com>
2026-04-17 03:03:57 +0200

    update submodules and stuff
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- nvim/.config/nvim/after/lsp/emmylua_ls.lua
+++ nvim/.config/nvim/after/lsp/emmylua_ls.lua
@@ -16,7 +16,7 @@ return {
                 library = {
                     vim.env.VIMRUNTIME,
                     '${3rd}/luv/library',
-                    '${3rd}/busted/library'
+                    '${3rd}/busted/library',
                 },
             },
             completion = {
