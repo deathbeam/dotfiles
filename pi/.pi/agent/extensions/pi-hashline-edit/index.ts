afc859bc2427dec941f15ee613e5e4281d32089d
Tomas Slusny <slusnucky@gmail.com>
2026-09-20 19:27:06 +0200

    small cleanup
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- pi/.pi/agent/extensions/pi-hashline-edit/index.ts
+++ pi/.pi/agent/extensions/pi-hashline-edit/index.ts
@@ -6,13 +6,5 @@ import { registerReadTool } from "./src/read";
 export default function (pi: ExtensionAPI): void {
 	registerReadTool(pi);
 	registerEditTool(pi);
-	// Self-gates on ripgrep being available on PATH.
 	registerGrepTool(pi);
-
-	pi.on("session_start", async (_event, ctx) => {
-		const debugValue = process.env.PI_HASHLINE_DEBUG;
-		if (debugValue === "1" || debugValue === "true") {
-			ctx.ui.notify("Hashline Edit mode active", "info");
-		}
-	});
 }
